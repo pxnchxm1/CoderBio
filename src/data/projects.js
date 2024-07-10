b44c0c4855001684bdaa1418e765d1823d6feb2b
pxnchxm1 <you@example.companchamipkumar1995@gmail.com>
2024-07-10 14:46:01 +0530

    fix:tree_structure

--- src/data/projects.js
+++ src/data/projects.js
@@ -11,7 +11,7 @@ export const projects=[
         title:"CineSLICE",
         category:"React Projects",
         description:"This web application, built with React.js, Vite, and Firebase, serves as a platform for users to explore and watch trailers of various movies. Utilizing data from the TMDB (The Movie Database) API, users can discover trending, popular, and upcoming movies, and watch their trailers directly on the platform.",
-        image:"../src/assets/images/cineslice.jpg",
+        image:"../public/assets/images/cineslice.jpg",
         siteUrl:"https://cine-slice-8t3w.vercel.app/",
         
         technologies:[FaReact ,IoLogoFirebase ,SiMui ,DiJavascript ,FaCss3Alt,TbBrandVite  ]
@@ -22,7 +22,7 @@ export const projects=[
         title:"CuriousBees",
         category:"Flutter Projects",
         description:"The Research Collaboration Hub is a cutting-edge platform leveraging Flutter, Firestore, Firebase, and Python to enable seamless project collaboration, scheduling, and communication among researchers and professionals. Users can foster efficient research management and networking capabilities within a user-friendly mobile interface.",
-        image:"../src/assets/images/bees1.jpg",
+        image:"../public/assets/images/bees1.jpg",
         siteUrl:"https://srm-curious-bug.pages.dev/",
         technologies:[RiFlutterFill,SiDart ,IoLogoFirebase]
        
@@ -32,7 +32,7 @@ export const projects=[
         title:"SRMTT",
         category:"Flutter Projects",
         description:"Built web-based timetable generator (Flutter/Python) empowering students and faculty to get custom daily schedules through intuitive interface.",
-        image:"../src/assets/images/srmtt.jpg",
+        image:"../public/assets/images/srmtt.jpg",
         siteUrl:"https://srmtt.livewires.tech/",
         technologies:[RiFlutterFill,SiDart ,IoLogoFirebase,FaPython],
         
